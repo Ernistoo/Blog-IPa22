3ecc2707e46e23d6646e5b4cf2010c9fe43da3a8
Your Name <you@example.com>
2024-03-14 14:47:47 +0200

    merkakis

--- index.php
+++ index.php
@@ -1,16 +1,15 @@
 <?php
 
-echo "hi";
 
 $url = parse_url($_SERVER["REQUEST_URI"])["path"];
 // dd($_SERVER["REQUEST_URI"]);
 
 if ($url == "/") {
     require "controllers/index.php";
-}
-if ($url == "/about") {
+} else if ($url == "/about") {
     require "controllers/about.php";
-}
-if ($url == "/story") {
+} else if ($url == "/story") {
     require "controllers/story.php";
+} else {
+    require "controllers/404.php";
 }
---
+++ views/404.view.php
@@ -0,0 +1,5 @@
+<?php require "components/head.php" ?>
+
+
+<h1 class="yeye" style="color:red; text-align:center">Retards! Aizej atpakal uz pamatskolu un apgusti primaru domasanu tu merkaki!</h1>
+<?php require "components/footer.php" ?>
